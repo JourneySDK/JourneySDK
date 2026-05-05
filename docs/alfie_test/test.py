e12356285ba07307299f1910bda73b43e3b538db
Piotr Sliwa <iodosite@gmail.com>
2026-05-06 01:59:50 +0200

    wip

--- docs/alfie_test/test.py
+++ docs/alfie_test/test.py
@@ -1,17 +1,22 @@
 from __future__ import annotations
 
 from journeysdk import journey, step
-from journeysdk.tools.playwright import open_page
+from journeysdk.tools.playwright import JourneyPlaywrightPage, open_page
 
 
-def first():
+def first() -> JourneyPlaywrightPage:
     page = open_page("https://app.staging.heyalfie.com/", headless=False)
-    result = page.prompt('Sign in as e2etest@heyalfie.com using password "1111". Expect no errors.', memory='sign-in')
-    # should never reach here
+    page.prompt('Sign in as e2etest@heyalfie.com using password "1111". Expect no errors.', memory='sign-in')
+    return page
+
+
+def start_chatting(saved_page: JourneyPlaywrightPage) -> JourneyPlaywrightPage:
+    page = open_page(saved_page, headless=False)
     page.prompt('start chatting with Alfie - say you need to fix a toilet')
     return page
 
 
 @journey
 def playwright_resume_journey() -> None:
-    step(first)
+    page = step(first)
+    step(start_chatting, page)
